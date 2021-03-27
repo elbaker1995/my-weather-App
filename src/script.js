82590e7a4e41e5404a786918bbb2ba530e223e9c
elbaker1995 <elbaker1995@gmail.com>
2021-03-27 22:21:03 +0000

    Updated main icon

--- src/script.js
+++ src/script.js
@@ -1,8 +1,13 @@
 let now = new Date();
-let time = document.querySelector("#time");
 let date = now.getDate();
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let year = now.getFullYear();
 
 let months = [
@@ -19,7 +24,8 @@ let months = [
   "DEC",
 ];
 let month = months[now.getMonth()];
-time.innerHTML = `${hours}:${minutes} ${date}${month}${year}  `;
+document.querySelector("#time").innerHTML = `${hours}:${minutes}`;
+document.querySelector("#date").innerHTML = `${date}${month}${year}`;
 
 function search(city) {
   let apiKey = "d12bd95cd8fc2d137ab72261317f84d8";
@@ -40,6 +46,13 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  let iconElement = document.querySelector("#main-icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 
 function handleSubmit(event) {
